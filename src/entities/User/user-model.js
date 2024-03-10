ebb09dd042938a24ad6d6a31684a306faa9e2c95
Eryhnar <pedrofernbel@gmail.com>
2024-03-10 15:22:17 +0100

    feat: User model created.

---
+++ src/entities/User/user-model.js
@@ -0,0 +1,60 @@
+import { Schema, model } from "mongoose"
+
+const UserSchema = new Schema(
+  {
+    userHandle: {
+      type: String,
+      required: false,
+    },
+    userName: {
+        type: String,
+        required: true,
+        default: userHandle
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    role: {
+      type: String,
+      enum: ["user", "admin", "super_admin"],
+      default: "user",
+    },
+    profilePicture: {
+      type: String,
+      required: true,
+      default: "https://upload.wikimedia.org/wikipedia/commons/2/2c/Default_pfp.svg",
+    },
+    bio: {
+        type: String,
+        required: false,
+    },
+    followers: [{
+      type: Schema.Types.ObjectId,
+      ref: 'User'
+    }],
+    following: [{
+      type: Schema.Types.ObjectId,
+      ref: 'User'
+    }],
+    // favouriteBooks: [
+    //   {
+    //     type: Schema.Types.ObjectId,
+    //     ref: 'Book'
+    //   }
+    // ]
+  },
+  {
+    timestamps: true,
+    versionKey: false,
+  }
+)
+
+const User = model("User", UserSchema)
+
+export default User
