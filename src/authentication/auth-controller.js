baa1aadfea76c5359e4b07bad366843fae2b27d7
Eryhnar <pedrofernbel@gmail.com>
2024-03-10 17:08:19 +0100

    feat: register route connected to the db.

--- src/authentication/auth-controller.js
+++ src/authentication/auth-controller.js
@@ -1,16 +1,53 @@
-
+import User from "../entities/User/user-model.js";
 
 export const register = async (req, res) => {
     try {
+        const { userName, email, password } = req.body;
+
+        if (!userName || !email || !password) {
+            return res.status(400).json(
+                { 
+                    success: false,
+                    message: "All fields are required"
+                }
+            );
+        }
+
+        const userHandle = "@" + userName.trim().toLowerCase();
+
+        if (await User.findOne ({ userHandle })) {
+            return res.status(400).json(
+                { 
+                    success: false,
+                    message: "User already exists"
+                }
+            );
+        }
+
+        if (await User.findOne ({ email })) {
+            return res.status(400).json(
+                { 
+                    success: false,
+                    message: "User with this email already exists"
+                }
+            );
+        }
+
+        const user = await User.create({ userHandle, userName, email, password });
+
         res.status(201).json(
             { 
-                message: "User created successfully" 
+                success: true,
+                message: "User created successfully",
+                data: user
             }
         );
     } catch (error) {
         res.status(500).json(
             { 
-                message: "Internal server error" 
+                success: false,
+                message: "Internal server error",
+                error: error.message
             }
         );
     }
