b7241d773ed2fe5cef698d7f96d84e1f82c8a29e
Eryhnar <pedrofernbel@gmail.com>
2024-03-14 22:39:06 +0100

    feat: added basic updatePost route.

--- src/entities/Post/post-controller.js
+++ src/entities/Post/post-controller.js
@@ -153,3 +153,22 @@ export const createPost = async (req, res) => {
         );
     }
 }
+
+export const updatePost = async (req, res) => {
+    try {
+        res.status(200).json(
+            { 
+                success: true,
+                message: "Post updated successfully",
+            }
+        );
+    } catch (error) {
+        res.status(500).json(
+            { 
+                success: false,
+                message: "Internal server error",
+                error: error.message
+            }
+        );
+    }
+}
--- src/entities/Post/post-routes.js
+++ src/entities/Post/post-routes.js
@@ -1,12 +1,13 @@
 import { Router } from "express";
 import { auth } from "../../middlewares/auth.js";
 import { isSuperadmin } from "../../middlewares/isSuperadmin.js";
-import { createPost, getPosts, getTimeline } from "./post-controller.js";
+import { createPost, getPosts, getTimeline, updatePost } from "./post-controller.js";
 
 const router = Router();
 
 router.get('/', auth, isSuperadmin, getPosts); //admin
 router.get('/timeline', auth, getTimeline);
 router.post('/', auth, createPost);
+router.put("/" , auth, updatePost);
 
 export default router;
