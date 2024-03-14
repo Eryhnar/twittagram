e4ea7b99d12057e91a7b5b1ef76dce178eae6a48
Eryhnar <pedrofernbel@gmail.com>
2024-03-14 18:55:43 +0100

    feat: added logic to getPosts.

--- src/entities/Post/post-controller.js
+++ src/entities/Post/post-controller.js
@@ -1,10 +1,29 @@
+import Post from "./post-model.js";
 
 export const getPosts = async (req, res) => {
     try {
+        //const limit = parseInt(req.query.limit) || 10;
+        const page = req.query.page || 1;
+        const limit = 10;
+        if (isNaN(page) || page <= 0) {
+            return res.status(400).json(
+                { 
+                    success: false,
+                    message: "Invalid page number"
+                }
+            );
+        }
+
+        const skip = (page - 1) * limit;
+
+        const posts = await Post.find().skip(skip).limit(limit);
+
         res.status(200).json(
             { 
                 success: true,
-                message: "Get all posts"
+                message: "Get all posts",
+                data: posts,
+                page
             }
         );
     } catch (error) {
